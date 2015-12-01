36eb02532bbb5edd25ed94f26ef0c82b50cc221b
Alexandr <duden89@gmail.com>
2015-12-01 16:09:22 +0200

    Инициализация проекта.

---
+++ ImageToByteConverter/ByteImageConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ImageToByteConverter
+{
+    class ByteImageConverter
+    {
+        public static ImageSource ByteToImage(byte[] imageData)
+        {
+            BitmapImage biImg = new BitmapImage();
+            MemoryStream ms = new MemoryStream(imageData);
+            biImg.BeginInit();
+            biImg.StreamSource = ms;
+            biImg.EndInit();
+
+            ImageSource imgSrc = biImg as ImageSource;
+
+            return imgSrc;
+        }
+
+        public static string ImageToByte(FileStream fs)
+        {
+            byte[] imgBytes = new byte[fs.Length];
+            fs.Read(imgBytes, 0, Convert.ToInt32(fs.Length));
+            string encodeData = Convert.ToBase64String(imgBytes, Base64FormattingOptions.InsertLineBreaks);
+
+            return encodeData;
+        }
+    }
+}
---
+++ ImageToByteConverter/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace ImageToByteConverter
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofg = new OpenFileDialog();
+            ofg.Title = "Select an image";
+            ofg.Filter = "Images (.jpg, .png, .gif, .bmp)|*.jpg;*.png;*.gif;*.bmp";
+            ofg.Multiselect = false;
+
+            if (ofg.ShowDialog() == true)
+            {
+                if (ofg.FileName != null && ofg.FileName.Length > 0)
+                {
+                    ofg.OpenFile();
+                    FileStream fs = new FileStream(ofg.FileName, FileMode.Open, FileAccess.Read);
+                    TextBox.Text = ByteImageConverter.ImageToByte(fs);
+
+                    byte[] imgStr = Convert.FromBase64String(TextBox.Text);
+                    Image.Source = ByteImageConverter.ByteToImage(imgStr);
+                }
+            }
+        }
+    }
+}
